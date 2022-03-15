fc5e2b6229daa97c282aac97b377144eebe404bd
mo36924 <mo36924@users.noreply.github.com>
2022-03-16 02:17:42 +0900

    🐛

--- src/jsx.d.ts
+++ src/jsx.d.ts
@@ -7,6 +7,13 @@ type JSXIntrinsicElements = {
 };
 
 declare namespace JSX {
+  interface Element extends globalThis.JSX.Element {}
+  interface ElementClass extends globalThis.JSX.ElementClass {}
+  interface ElementAttributesProperty extends globalThis.JSX.ElementAttributesProperty {}
+  interface ElementChildrenAttribute extends globalThis.JSX.ElementChildrenAttribute {}
+  interface LibraryManagedAttributes<C, P> extends globalThis.JSX.LibraryManagedAttributes<C, P> {}
+  interface IntrinsicAttributes extends globalThis.JSX.IntrinsicAttributes {}
+  interface IntrinsicClassAttributes<T> extends globalThis.JSX.IntrinsicClassAttributes<T> {}
   interface IntrinsicElements extends JSXIntrinsicElements {}
 }
 
